ef63f0d8c8373deb764d91d027ec17ca0856b069
sachinumans <sachinumans@gmail.com>
2022-05-25 12:26:31 +0200

    Started on 1 and 2

---
+++ ExtractGivenData.m
@@ -0,0 +1,28 @@
+% clear all; 
+close all; clc;
+% Load cvs
+if ~exist("D", "var"); D = readtable("data.csv"); end
+N = size(D,1);
+idx_start = 1; % index of day start
+dayCount = 1; % Day index
+dayArr = convertCharsToStrings(D{:, 1});
+
+datCell = cell([], 1); % Datacell with a cell for each day
+
+figure(); hold on;
+
+while idx_start <= N
+    day = convertCharsToStrings(D{idx_start,1});
+    idx_end = find(dayArr == day, 1, 'last' ); % index of day end
+    
+    Y = table2array(D(idx_start:idx_end, 3));
+    plot(Y)
+    
+    datCell{dayCount} = Y;
+    
+    idx_start = idx_end+1;
+    
+    dayCount = dayCount +1;
+end
+
+save datCell.mat datCell
