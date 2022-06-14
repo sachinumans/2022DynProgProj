816ba69b02fbda1d307c105ce13d8d371343f6f9
sachinumans <sachinumans@gmail.com>
2022-06-14 13:57:28 +0200

    Extracted data and defined system dynamics

--- ExtractGivenData.m
+++ ExtractGivenData.m
@@ -1,7 +1,5 @@
-% clear all; 
-close all; clc;
-% Load cvs
-if ~exist("D", "var"); D = readtable("data.csv"); end
+function [datCell, oneYear, ddt_oneYear, M, S] = ExtractGivenData(D, plt)
+
 N = size(D,1);
 idx_start = 1; % index of day start
 dayCount = 1; % Day index
@@ -13,15 +11,15 @@ oneYear = table2array(D(:, 3));
 
 datCell = cell([], 1); % Datacell with a cell for each day
 
-figure(); hold on;
-title("All days on top of each other")
+if plt=="plot"; figure(); hold on; title("All days on top of each other"); end
 
 while idx_start <= N
     day = convertCharsToStrings(D{idx_start,1});
     idx_end = find(dayArr == day, 1, 'last' ); % index of day end
     
     Y = table2array(D(idx_start:idx_end, 3));
-    plot(Y)
+    
+    if plt=="plot"; plot(Y); end
     
     datCell{dayCount} = Y;
     dayStartIdx{dayCount} = idx_start;
@@ -31,29 +29,18 @@ while idx_start <= N
     
     dayCount = dayCount +1;
 end
-hold off
-save datCell.mat datCell oneYear
-
-%% Plot the entire year
-figure(); hold on
-title("The entire year")
-days = (1:N).*365./N;
-plot(days, oneYear);
-hold off
-
-%% Plot derivative of the entire year
-figure(); hold on
-title("The derivate of entire year")
-ddt_oneYear = diff(oneYear);
-plot(days(2:end), ddt_oneYear);
-hold off
+if plt=="plot"; hold off; end
+% save datCell.mat datCell oneYear
 
 %% Plot derivative of each day
 n = max(size(datCell{1}));
 X = zeros(max(size(datCell)), n);
+ddt_oneYear = diff(oneYear);
 
-figure(); hold on
-title("The derivative of each day on top of each other")
+if plt=="plot"
+    figure(); hold on; 
+    title("The derivative of each day on top of each other")
+end
 for d = 1:(dayCount-1)
     idxStart = dayStartIdx{d};
     idxEnd = dayEndIdx{d};
@@ -62,22 +49,43 @@ for d = 1:(dayCount-1)
     else
         X(d, :) = ddt_oneYear(idxStart:idxEnd);
     end
-    plot(ddt_oneYear(idxStart:min(idxEnd,N-1)))
+    if plt=="plot"; plot(ddt_oneYear(idxStart:min(idxEnd,N-1))); end
     
 end
-hold off
+if plt=="plot"; hold off; end
 
-%% Plot std of each minute of the day
+%% std of each minute of the day
 S = std(X);
-figure(); hold on
-title("The standard deviation of the derivative of each minute in the day")
-plot(S);
 
-hold off
-%% Plot mean of each minute of the day
+%% mean of each minute of the day
 M = mean(X, 1);
+
+%% Plots
+if plt=="plot"
+% Plot the entire year
+figure(); hold on
+title("The entire year")
+days = (1:N).*365./N;
+plot(days, oneYear);
+hold off
+
+% Plot derivative of the entire year
+figure(); hold on
+title("The derivate of entire year")
+plot(days(2:end), ddt_oneYear);
+hold off
+
+% Plot mean of deriv
 figure(); hold on
 title("The mean of the derivative of each minute in the day")
 plot(M);
-
 hold off
+
+% Plot std of deriv
+figure(); hold on
+title("The standard deviation of the derivative of each minute in the day")
+plot(S);
+hold off
+
+end
+end
--- Main.m
+++ Main.m
@@ -1,6 +1,6 @@
-clear all; close all; clc;
+clearvars -except D; close all; clc; tic
+plt = "plot";
 %% Define vars
-
 cmax = 7; %kWh
 ubAlpha = 0.96;
 lbAlpha = 0.87;
@@ -11,7 +11,30 @@ ppeak = 1.50; % euro
 s=whos;
 vars = cell2struct({s.name}.',{s.name});
 
-%% Load data
-load datCell
+%% Load cvs
+if ~exist("D", "var"); D = readtable("data.csv"); end
 
-%%
+[datCell, oneYear, ddt_oneYear, M, S] = ExtractGivenData(D, plt);
+
+%% Determine characteristics of randomness
+Zmean = mean(ddt_oneYear);
+Zstd = std(ddt_oneYear);
+
+Z = makedist('Normal','mu',Zmean,'sigma',Zstd);
+
+if plt=="plot"
+    d = -1.5:0.03:1.5;
+    dNeg = d(d<=0);
+    dPos = d(d>=0);
+    figure(); hold on
+    plot([Zmean Zmean], [0 pdf(Z,Zmean)], "k:")
+    fill([fliplr(dNeg) dNeg],[zeros(1, size(dNeg,2)) pdf(Z, dNeg)], 'r', 'facealpha',.5)
+    fill([fliplr(dPos) dPos],[zeros(1, size(dPos,2)) pdf(Z, dPos)], 'g', 'facealpha',.5)
+    text(-.25, 0.5, num2str(cdf(Z, 0), 4))
+    text(.15, 0.5, num2str(1-cdf(Z, 0), 4))
+    title("Power consumption derivative")
+    xlabel("Z")
+    ylabel("P(Z)")
+end
+
+toc
